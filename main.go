b3d4c1f90679d35c8a812f59add3b31728d9fd45
Sachin47singh <109524881+Sachin47singh@users.noreply.github.com>
2022-09-15 12:43:20 +0530

    Add files via upload

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/Sachin47singh/go-course/pkg/config"
 	"github.com/Sachin47singh/go-course/pkg/handlers"
 	"github.com/Sachin47singh/go-course/pkg/render"
+	"github.com/alexedwards/scs/v2"
 )
 
 // const portNumber = ":8080"
@@ -17,14 +19,60 @@ import (
 // 	http.HandleFunc("/", handlers.Home)
 // 	http.HandleFunc("/about", handlers.About)
 
-// 	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
-// 	_ = http.ListenAndServe(portNumber, nil)
-// }
-const portNumber = ":1010"
+//		fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+//		_ = http.ListenAndServe(portNumber, nil)
+//	}
+// const portNumber = ":1011"
+
+// main is the main function
+// func main() {
+// 	var app config.AppConfig
+
+// 	tc, err := render.CreateTemplateCache()
+// 	if err != nil {
+// 		log.Fatal("cannot create template cache")
+// 	}
+
+// 	app.TemplateCache = tc
+// 	app.UseCache = false
+
+// 	repo := handlers.NewRepo(&app)
+// 	handlers.NewHandlers(repo)
+
+// 	render.NewTemplates(&app)
+
+// 	// http.HandleFunc("/", handlers.Repo.Home)
+// 	// http.HandleFunc("/about", handlers.Repo.About)
+
+//		fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+//		// _ = http.ListenAndServe(portNumber, nil)
+//		srv := &http.Server{
+//			Addr:    portNumber,
+//			Handler: routes(&app),
+//		}
+//		err = srv.ListenAndServe()
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//	}
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
 
 // main is the main function
 func main() {
-	var app config.AppConfig
+	// change this to true when in production
+	app.InProduction = false
+
+	// set up the session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
@@ -39,11 +87,15 @@ func main() {
 
 	render.NewTemplates(&app)
 
-	http.HandleFunc("/", handlers.Repo.Home)
-	http.HandleFunc("/about", handlers.Repo.About)
-
 	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
-	_ = http.ListenAndServe(portNumber, nil)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
-
-
---
+++ routes.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/Sachin47singh/go-course/pkg/config"
+	"github.com/Sachin47singh/go-course/pkg/handlers"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	// mux := pat.New()
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+	return mux
+}
