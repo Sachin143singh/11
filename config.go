2b8574d4a7e6705635cc6689599c4c02cbc5e4f4
Sachin47singh <109524881+Sachin47singh@users.noreply.github.com>
2022-09-15 12:42:44 +0530

    Add files via upload
    22

--- config.go
+++ config.go
@@ -1,8 +1,24 @@
+// package config
+
+// import (
+// 	"html/template"
+// 	"log"
+// )
+
+// // AppConfig holds the application config
+// type AppConfig struct {
+// 	UseCache      bool
+// 	TemplateCache map[string]*template.Template
+// 	InfoLog       *log.Logger
+// }
+
 package config
 
 import (
 	"html/template"
 	"log"
+
+	"github.com/alexedwards/scs/v2"
 )
 
 // AppConfig holds the application config
@@ -10,4 +26,6 @@ type AppConfig struct {
 	UseCache      bool
 	TemplateCache map[string]*template.Template
 	InfoLog       *log.Logger
+	InProduction  bool
+	Session       *scs.SessionManager
 }
